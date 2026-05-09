fcd89cec7456b5d2f32dcea27c28bb5864b1c943
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-09 15:48:01 -0600

    Add: sql-update-all - submission-1

---
+++ SQL/sql-update-all/submission-1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+INSERT INTO students (id, name, age) VALUES
+(1, 'Alice', 20),
+(2, 'Bob', NULL),
+(3, 'Charlie', 30);
+-- Do not modify above this line. --
+
+UPDATE students SET age = NULL;
+
+
+
+-- Do not modify below this line. --
+SELECT * FROM students;
