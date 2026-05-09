7d1ae41b1ae9c4bb370268681823e235309824b3
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-09 16:06:34 -0600

    Add: sql-delete-returning - submission-1

---
+++ SQL/sql-delete-returning/submission-1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE bank_accounts (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    balance INTEGER,
+    last_transaction_date DATE
+);
+
+INSERT INTO bank_accounts (name, balance, last_transaction_date) VALUES
+    ('Alice', 1000, '2023-11-30'),
+    ('Bob', -100, '2023-12-31'),
+    ('Charlie', 2000, '2024-01-03'),
+    ('David', -500, '2024-01-04'),
+    ('Eve', 1500, '2024-01-05');
+-- Do not modify above this line. --
+
+
+DELETE FROM bank_accounts 
+WHERE balance < 0 AND last_transaction_date < '2024-01-01'::DATE 
+RETURNING id, name; 
+
+
