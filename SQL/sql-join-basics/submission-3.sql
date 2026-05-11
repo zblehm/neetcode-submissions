c668732fa3584afbaa0c5c3451b70a6943204e74
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-11 09:42:48 -0600

    Add: sql-join-basics - submission-3

---
+++ SQL/sql-join-basics/submission-3.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    total_price DECIMAL(10, 2) NOT NULL
+);
+
+INSERT INTO users (user_id, name) VALUES
+(1, 'John'),
+(2, 'Jane');
+
+INSERT INTO orders (order_id, user_id, total_price) VALUES
+(1, 1, 100.00),
+(2, 2, 200.00);
+-- Do not modify above this line. --
+
+SELECT users.user_id, name, order_id, total_price
+FROM users
+JOIN orders
+    ON users.user_id = orders.order_id;
+
+
+
+
