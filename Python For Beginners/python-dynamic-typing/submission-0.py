a3f07101e569a06ccf02b0b8662e2eb315b6d266
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-12 07:37:48 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 3
+print(type(variable))
+variable = 3.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = 'string'
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
