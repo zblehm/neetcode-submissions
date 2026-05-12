2b5a223d5e8dd46edcd0be239c1043de5d255a13
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-12 07:38:52 -0600

    Add: python-type-casting - submission-0

---
+++ Python For Beginners/python-type-casting/submission-0.py
@@ -0,0 +1,10 @@
+pi = 3.141592653589793
+square_root_8 = 2.8284271247461903
+# Do not modify above this line
+
+pi = int(pi)
+square_root_8 = int(square_root_8)
+
+# Do not modify below this line
+print(pi)
+print(square_root_8)
