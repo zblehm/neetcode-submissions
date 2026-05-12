cbf715bf5280c851c5159ca2e7ce120ffb9aede7
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-12 07:29:09 -0600

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+python_creation_date = "February, 1991"
+java_creation_date = "May, 1995"
+javascript_creation_date = "December, 1995"
+
+print(python_creation_date)
+print(java_creation_date)
+print(javascript_creation_date)
