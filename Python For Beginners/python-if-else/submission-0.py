5f4b00e20b23ba522e04cad0ee02eb31e59d4873
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-13 13:34:08 -0600

    Add: python-if-else - submission-0

---
+++ Python For Beginners/python-if-else/submission-0.py
@@ -0,0 +1,10 @@
+def get_min(a: int, b: int) -> int:
+    if a <= b:
+        return a
+    else:
+        return b
+
+# don't modify code below this line
+print(get_min(10, 11))
+print(get_min(5, -7))
+print(get_min(20, 20))
