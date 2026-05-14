fc87bbced8a09bb1421cbe923d0c5fcec14381d7
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-14 06:35:01 -0600

    Add: python-control-flow - submission-0

---
+++ Python For Beginners/python-control-flow/submission-0.py
@@ -0,0 +1,18 @@
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    break
+    print(i)
+
+for i in range(1, 8):
+    continue
+    print(i)
+
+print("nothing else happened")
