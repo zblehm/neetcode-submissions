9aaae3ed9703c7faa7715f5cac360407ca0ac259
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-14 06:40:52 -0600

    Add: python-string-indexing - submission-0

---
+++ Python For Beginners/python-string-indexing/submission-0.py
@@ -0,0 +1,18 @@
+def print_first_char(word: str) -> None:
+    print(word[0])
+
+def print_second_char(word: str) -> None:
+    print(word[1])
+
+def print_last_char(word: str) -> None:
+    print(word[-1])
+
+
+# do not modify below this line
+print_first_char("hello")
+print_second_char("hello")
+print_last_char("hello")
+
+print_first_char("yay")
+print_second_char("yay")
+print_last_char("yay")
