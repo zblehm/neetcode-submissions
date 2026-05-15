e31bf7eee840bfc29792b3f91ccd7db27f2427ab
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-15 06:55:32 -0600

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,21 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+# Task 1: print the dictionary
+print(your_dict)
+
+# Task 2: print the value of key "a"
+print(your_dict["a"])
+
+# Task 3: Print True/False based on a key of "d" in the dictionary
+print("d" in your_dict)
+
+#Task 4: Reassign the value of key "a" to 4.
+your_dict["a"] = 4
+
+# Task 5: print the dictionary again
+print(your_dict)
+
