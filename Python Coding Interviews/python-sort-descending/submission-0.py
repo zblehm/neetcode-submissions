ecb4efc0655f13478fa1b8274071277a6e402996
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-26 16:25:22 -0600

    Add: python-sort-descending - submission-0

---
+++ Python Coding Interviews/python-sort-descending/submission-0.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    # return the list in descending order
+    words.sort(reverse = True)
+    return words
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    # return the list in descending order
+    numbers.sort(reverse = True)
+    return numbers
+
+def sort_decimals(numbers: List[float]) -> List[float]:
+    # return the list in descending order
+    numbers.sort(reverse = True)
+    return numbers
+
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, 5, 3, 2, 4, 11, 19, 9, 2, 5, 6, 7, 4, 2, 6]))
+
+print(sort_decimals([3.14, 2.82, 6.433, 7.9, 21.555, 21.554]))
