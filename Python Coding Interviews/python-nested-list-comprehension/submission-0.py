8000924fd9c11c4f4d7a3c1f96d7ab1030751374
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-28 11:14:52 -0600

    Add: python-nested-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-nested-list-comprehension/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def create_grid(rows: int, cols: int, value: int) -> List[List[int]]:
+    return [[value] * cols for _ in range(rows)]
+
+
+# do not modify below this line
+print(create_grid(2, 3, 0))
+print(create_grid(3, 2, 1))
+print(create_grid(4, 4, 4))
+print(create_grid(1, 1, 5))
+print(create_grid(1, 5, 5))
