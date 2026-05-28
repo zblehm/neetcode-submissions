892ff467cfd1f47d1c62d9ec33b4a0e5336b0e36
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-28 10:46:35 -0600

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    arr.reverse()
+    return arr
+
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
