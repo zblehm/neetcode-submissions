690c5fdfcc03c8decb6b7c9aacfe829c5823e929
zblehm <153874893+zblehm@users.noreply.github.com>
2026-05-29 17:21:41 -0600

    Add: python-heap-n-largest - submission-0

---
+++ Python Coding Interviews/python-heap-n-largest/submission-0.py
@@ -0,0 +1,34 @@
+import heapq
+from typing import List
+
+
+def get_max_element(arr: List[int]) -> int:
+    return heapq.nlargest(1, arr)[0]
+
+
+def get_max_4_elements(arr: List[int]) -> List[int]:
+    # Return elements in *decreasing* order
+    return heapq.nlargest(4, arr)
+
+
+def get_max_2_elements(arr: List[int]) -> List[int]:
+    # Return elements in *increasing* order
+    two_max = heapq.nlargest(2, arr)
+    two_max.reverse()
+    return two_max
+
+
+
+# do not modify below this line
+print(get_max_element([1, 2, 3]))
+print(get_max_element([3, 2, 1, 4, 6, 2]))
+print(get_max_element([1, 9, 7, 3, 2, 1, 4, 6, 2]))
+
+print(get_max_4_elements([4, 9, 7, 3, 2, 7, 4, 6, 2]))
+print(get_max_4_elements([4, 9, 7, 2, 1, 3, 2, 3, 4, 6, 2, 3]))
+print(get_max_4_elements([4, 7, 2, 3, 2, 4, 6, 2]))
+
+print(get_max_2_elements([4, 5, 3, 7]))
+print(get_max_2_elements([8, 8, 7, 9]))
+print(get_max_2_elements([1, 2, 3, 9, 8, 7, 6]))
+
